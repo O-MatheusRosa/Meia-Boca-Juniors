a99ebbeb322a22182150a365117b80c225ea7e84
Matheus J. Rosa <joserosamatheus3@gmail.com>
2026-05-18 21:45:51 -0300

    Arrumando a cagada da organização, tinha deixado fora da pasta kkkkkkk

---
+++ figurinha.h
@@ -0,0 +1,36 @@
+#ifndef FIGURINHA_H
+#define FIGURINHA_H
+
+//###########################################################//
+//Estrutura
+//###########################################################//
+
+typedef struct{
+   
+   char codigo[15]; //codigo da figurinha
+   char titulo[60]; //Nome do jogador
+   char secao[60];  //categoria do jogador ou pg, ainda tem q ver
+   char grupo[60];  //grupo q o jogador esta, tipo o Brasil é gp B,logo o jogador tbm
+   char tipo[30];   //Raridade
+
+   //controle de fluxo das fig
+
+   int colada;               //pra ser mais facil, 1 ta colado, 0 nao ta
+   int quantidade_repetidas; //quantas da mesma o jogador tem
+
+}Dados_Figurinha;
+
+//****************************//
+//estruct do album
+//****************************//
+typedef struct{
+
+    //aloca dinamicamente o album
+
+    Dados_Figurinha *vetor; 
+    int quantidade_atual;
+    int quantidade_maxima;
+
+}Album;
+
+#endif
