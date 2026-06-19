6e71ca6108f71df3383944507125569aedd3ae93
rafael-boni <bonir6691@gmail.com>
2026-06-19 14:05:22 -0300

    inckuindo a fucao utfpr

--- include/textura.h
+++ include/textura.h
@@ -1,6 +1,22 @@
 #ifndef TEXTURA_H
 #define TEXTURA_H
 
+#include "raylib.h"
+
+// Definição da estrutura de questões do seu CSV
+typedef struct {
+    int id;
+    char nivel[30];
+    char theme[50];
+    char enunciado[400];
+    char altA[250];
+    char altB[250];
+    char altC[250];
+    char altD[250];
+    char correta[10];
+} Questao;
+
+
 int Tela_Home(Texture2D fundo, Music musica);
 void Tela_Jogo(Texture2D fundo_jogo, Music musica);
 int Tela_Diltu(Music musica, float *saldo_jogador);
@@ -8,6 +24,8 @@ void Tela_Aposta(Music musica,float *saldo_jogador);
 void Tela_Futebol(Music musica,float *saldo_jogador);
 void Tela_Bixo(Music musica,float *saldo_jogador);
 
-
+void ExecutarModuloMonitoria(float *saldo_jogador);
+void fatiar_csv_avancado(char *linha, char colunas[9][400]);
+int carregar_questoes_csv(Questao *banco);
 
 #endif
--- src/textura.c
+++ src/textura.c
@@ -37,7 +37,7 @@ int Tela_Home(Texture2D fundo, Music musica){
                 DrawTextEx(fonte_brasil,"JOGAR",posicao_texto,40,2,BLACK);
             }//final do else
             
-            //DrawRectangleRec(botao_jogar, Fade(RED,0.5f));//um esboço pra saber onde ta o butaum
+            //DrawRectangleRec(botao_jogar, Fade(RED,0.5f));//um esboï¿½o pra saber onde ta o butaum
         EndDrawing();
     }//final da fnc
 
@@ -47,29 +47,29 @@ int Tela_Home(Texture2D fundo, Music musica){
 
 void Tela_Jogo(Texture2D fundo_jogo, Music musica){
 
-    // 1. ÁREA DA UTFPR (Canto Inferior Direito)
+    // 1. ï¿½REA DA UTFPR (Canto Inferior Direito)
     Vector2 utfpr[] = {
-        { 780, 430 },  // Ponto 1: Quina superior esquerda (começo da curva da rotatória)
-        { 1280, 430 }, // Ponto 2: Vai reto acompanhando a rua até a borda direita
-        { 1280, 720 }, // Ponto 3: Desce até o chão direito da tela
-        { 710, 720 },  // Ponto 4: Volta pelo chão até o limite da rua vertical
-        { 710, 500 },  // Ponto 5: Sobe reto acompanhando a calçada
-        { 780, 430 }   // Ponto 6: Faz a diagonal da rotatória e fecha
+        { 780, 430 },  // Ponto 1: Quina superior esquerda (comeï¿½o da curva da rotatï¿½ria)
+        { 1280, 430 }, // Ponto 2: Vai reto acompanhando a rua atï¿½ a borda direita
+        { 1280, 720 }, // Ponto 3: Desce atï¿½ o chï¿½o direito da tela
+        { 710, 720 },  // Ponto 4: Volta pelo chï¿½o atï¿½ o limite da rua vertical
+        { 710, 500 },  // Ponto 5: Sobe reto acompanhando a calï¿½ada
+        { 780, 430 }   // Ponto 6: Faz a diagonal da rotatï¿½ria e fecha
     };
     int quantidade_pontos = 6;
 
-    // 2. ÁREA DO DILTO (Canto Inferior Esquerdo)
+    // 2. ï¿½REA DO DILTO (Canto Inferior Esquerdo)
     Vector2 diltu[] = {
         { 0, 430 },    // Ponto 1: Borda esquerda da tela (logo abaixo da rua horizontal)
-        { 500, 430 },  // Ponto 2: Vai reto até começar a curvinha da rotatória
-        { 570, 500 },  // Ponto 3: Desce a diagonal da calçada
-        { 570, 720 },  // Ponto 4: Desce reto acompanhando a rua vertical até o chão
-        { 0, 720 },    // Ponto 5: Volta pelo chão até a borda esquerda da tela
+        { 500, 430 },  // Ponto 2: Vai reto atï¿½ comeï¿½ar a curvinha da rotatï¿½ria
+        { 570, 500 },  // Ponto 3: Desce a diagonal da calï¿½ada
+        { 570, 720 },  // Ponto 4: Desce reto acompanhando a rua vertical atï¿½ o chï¿½o
+        { 0, 720 },    // Ponto 5: Volta pelo chï¿½o atï¿½ a borda esquerda da tela
         { 0, 430 }     // Ponto 6: Sobe pela borda esquerda e fecha
     };
     int pontos_diltu = 6;
-    float saldo_jogador = 20000.00;//boni arruma aq dps fznd favor!!!!!!!
-
+    float saldo_jogador = 0;//boni arruma aq dps fznd favor!!!!!!!
+    //acabei de arumar aqui pink(18/06)
     while (!WindowShouldClose()){
         UpdateMusicStream(musica);
 
@@ -77,19 +77,20 @@ void Tela_Jogo(Texture2D fundo_jogo, Music musica){
             Vector2 mouse = GetMousePosition();
 
                 if (CheckCollisionPointPoly(mouse, utfpr, quantidade_pontos)) {
-                    //aqui é a fnc da facul dps, do boni no caso
+                    ExecutarModuloMonitoria(&saldo_jogador);
                 }//if da utfpr
 
                 if (CheckCollisionPointPoly(mouse, diltu, pontos_diltu)) {
                     Tela_Diltu(musica,&saldo_jogador);
+                    
                 }//if do diltuu
         }//fnc se clicar no butaum
 
         BeginDrawing();
         ClearBackground(BLACK);
         DrawTexture(fundo_jogo,0,0,WHITE);
-        //DrawLineStrip(diltu, pontos_diltu, RED);//teste pra ver onde é o clicavel o dilto
-        //DrawLineStrip(utfpr, quantidade_pontos, BLUE);//teste pra ver onde é o clicavel a utfpr
+        //DrawLineStrip(diltu, pontos_diltu, RED);//teste pra ver onde ï¿½ o clicavel o dilto
+        DrawLineStrip(utfpr, quantidade_pontos, BLUE);//teste pra ver onde ï¿½ o clicavel a utfpr
         EndDrawing();
     }//while
 }//parte grafica do jogo em si, o mapinha dele
